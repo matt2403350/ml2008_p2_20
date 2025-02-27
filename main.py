c7bccca525e7a4d990f684a4431f27bff6ff4b62
matt2403350 <2403350@sit.singaporetech.edu.sg>
2025-02-27 10:59:22 +0800

    First Commit Main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
+
+
